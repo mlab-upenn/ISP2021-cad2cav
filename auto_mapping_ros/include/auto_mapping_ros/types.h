34e5bc72fe6af336493356f1e57961c6f7444b20
Zhihao Ruan <ruanzh@seas.upenn.edu>
2021-03-30 03:24:45 -0400

    overload std hash function for Node type

--- auto_mapping_ros/include/auto_mapping_ros/types.h
+++ auto_mapping_ros/include/auto_mapping_ros/types.h
@@ -2,6 +2,7 @@
 #define AUTO_MAPPING_ROS_TYPES_H
 
 #include <array>
+#include <functional>
 #include <vector>
 
 /// Basic Cell of the Graph
@@ -22,6 +23,15 @@ struct Node {
 
     bool operator==(const Node &rhs) const;
     std::array<int, 2> get_location() const;
+};
+
+// Overload of C++ std template hash for customized class `Node`
+//  Reference: https://stackoverflow.com/a/17017281/9960905
+template <>
+struct std::hash<Node> {
+    std::size_t operator()(const Node &n) const {
+        return std::hash<int>()(n.id);
+    }
 };
 
 using Graph = std::vector<Node>;
