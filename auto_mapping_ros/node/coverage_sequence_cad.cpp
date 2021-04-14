c4b0fa1a19829bbe8e2e921358914ea4d1e66c3b
Zhihao Ruan <ruanzh@seas.upenn.edu>
2021-04-14 02:02:59 -0400

    switch coverage sequence creator to use METIS graph partition

--- auto_mapping_ros/node/coverage_sequence_cad.cpp
+++ auto_mapping_ros/node/coverage_sequence_cad.cpp
@@ -4,17 +4,23 @@
 #include <filesystem>
 namespace fs = std::filesystem;
 
-#include "../aco_router/utils.h"
-#include "../aco_router/vrp_solver.h"
 #include "auto_mapping_ros/graph_builder_bp.hpp"
 #include "auto_mapping_ros/utils.h"
-#include "fast_csv_parser/csv.h"
+#include "graph_partitioner/partitioner.hpp"
+#include "graph_partitioner/utils.hpp"
 
-int main(int argc, char const* argv[]) {
+int main(int argc, char** argv) {
     const fs::path amr_package_path = ros::package::getPath("auto_mapping_ros");
     const fs::path simulator_package_path = ros::package::getPath("f110_simulator");
     const fs::path cad_map_filepath = simulator_package_path / "maps/levine_4.dwg.png";
     const fs::path unreal_waypoints_path = amr_package_path / "csv/actorLocation.csv";
+    const std::string csv_filepath = amr_package_path / "csv/sequence";
+
+    ros::init(argc, argv, "coverage_sequence_generator", ros::InitOption::AnonymousName);
+    ros::NodeHandle n;
+    int n_agents = 0;
+    n.getParam("num_vehicles", n_agents);
+    ROS_WARN_STREAM("No. of vehicles: " << n_agents);
 
     cv::Mat map = cv::imread(cad_map_filepath, cv::IMREAD_GRAYSCALE);
     ROS_INFO_STREAM("Read in map of size (col*row): " << map.cols << "x" << map.rows);
@@ -22,23 +28,24 @@ int main(int argc, char const* argv[]) {
     amr::GraphBuilderBP graph_builder(map);
     graph_builder.build_graph(unreal_waypoints_path);
     auto graph = graph_builder.get_graph();
-    auto aco_graph = aco::convert_to_aco_graph(graph);
 
-    // Get Initial Point
-    int init_id = amr::get_closest_clicked_node_on_map(map, aco_graph);
+    const auto gp_graph = graph_partitioner::fromUserGraph(graph, true);
+    graph_partitioner::GraphPartitioner gp(gp_graph);
 
-    // Find Best Sequence
-    const std::string csv_filepath = amr_package_path / "csv/sequence";
-    const auto sequences_node = aco::solve_vrp(aco_graph, init_id);
+    // compute graph partition
+    auto subgraphs = gp.getPartition(n_agents, graph_partitioner::PartitionType::METIS);
 
-    for (int i = 0; i < sequences_node.first.size(); i++) {
-        std::vector<std::array<int, 2>> current_sequence{};
-        for (const auto& node : sequences_node.first[i]) {
-            current_sequence.emplace_back(std::array<int, 2>{static_cast<int>(node.x), static_cast<int>(node.y)});
-        }
+    // visualize subgraphs
+    for (unsigned int i = 0; i < subgraphs.size(); ++i) {
+        auto& subgraph = subgraphs.at(i);
+        double tsp_cost = subgraph.updateTSPSequence();
+        ROS_WARN("Updated TSP path cost for subgraph %d: %lf", i, tsp_cost);
+        const auto current_sequence = subgraph.getTSPSequence();
+
         const auto current_csv_filepath = csv_filepath + "_" + std::to_string(i + 1) + ".csv";
-        amr::write_sequence_to_csv(current_sequence, current_csv_filepath);
-        amr::visualize_sequence_on_graph(map, graph, current_sequence);
+        amr::write_sequence_to_csv(current_sequence, current_csv_filepath, true);
+
+        amr::visualize_sequence_on_graph(map, graph, current_sequence, true);
     }
 
     return 0;
