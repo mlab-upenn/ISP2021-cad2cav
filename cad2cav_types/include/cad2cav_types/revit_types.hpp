05fa23c2bf7e348b34f417c58e5a654a7f919766
Zhihao Ruan <ruanzh@seas.upenn.edu>
2021-10-10 02:11:50 -0400

    add revit types

--- cad2cav_types/include/cad2cav_types/revit_types.hpp
+++ cad2cav_types/include/cad2cav_types/revit_types.hpp
@@ -1,9 +1,12 @@
 #ifndef __CAD2CAV_TYPES_REVIT_TYPES_HPP__
 #define __CAD2CAV_TYPES_REVIT_TYPES_HPP__
 
+#include <Eigen/Dense>
 #include <algorithm>
 #include <cctype>
 #include <string>
+
+#include "cad2cav_types/line_segment2d.hpp"
 
 namespace cad2cav {
 
@@ -20,6 +23,34 @@ RevitObjectType RevitObjectTypeFromString(const std::string& data) {
     return RevitObjectType::WINDOW;
 }
 
+namespace revit {
+
+struct Wall {
+  LineSegment2D shape_;
+  double height_;  // unused
+  Wall() : shape_(), height_(-1.0) {}
+  Wall(double endpoint1_x, double endpoint1_y, double endpoint2_x,
+       double endpoint2_y, double height)
+      : shape_(endpoint1_x, endpoint1_y, endpoint2_x, endpoint2_y),
+        height_(height) {}
+  Wall(const Eigen::Vector2d& endpoint1, const Eigen::Vector2d& endpoint2,
+       double height)
+      : shape_(endpoint1, endpoint2), height_(height) {}
+};
+
+struct Door {
+  Eigen::Vector2d pos_;
+  double width_;
+  double height_;
+};
+
+struct Window {
+  Eigen::Vector2d pos_;
+  double width_;
+  double height_;
+};
+
+}  // namespace revit
 }  // namespace cad2cav
 
 #endif /* __CAD2CAV_TYPES_REVIT_TYPES_HPP__ */
