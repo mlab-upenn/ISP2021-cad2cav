2b74ecca5851cc753c4265c7ac5ae20faad48ee4
Zhihao Ruan <ruanzh@seas.upenn.edu>
2021-10-10 17:02:25 -0400

    add revit build map test

---
+++ map_service/test/revit_build_map.cpp
@@ -0,0 +1,17 @@
+#include <ros/ros.h>
+#include "map_service/map_server.hpp"
+#include "map_service/utils.hpp"
+#include "map_service/revit_info.hpp"
+
+int main(int argc, char **argv) {
+  ros::init(argc, argv, "map_revit_publisher_test");
+  cad2cav::map_service::RevitInfo revit_info =
+      cad2cav::map_service::utils::readRevitStructure("levine_2.csv");
+  cad2cav::map_service::MapServer map_server{0.05};
+  map_server.buildFromRevitInfo(revit_info);
+  while (ros::ok()) {
+    map_server.publishMap();
+    ros::spinOnce();
+  }
+  return 0;
+}
