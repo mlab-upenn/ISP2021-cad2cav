7df3365b8f1901c4cc54a9d88d99504515deaa98
Shumin326 <shumin@seas.upenn.edu>
2021-10-13 22:40:07 -0500

    Increased map resolution

--- map_service/test/revit_build_map.cpp
+++ map_service/test/revit_build_map.cpp
@@ -8,7 +8,7 @@ int main(int argc, char **argv) {
   ros::init(argc, argv, "map_revit_publisher_test");
   cad2cav::map_service::RevitInfo revit_info =
       cad2cav::map_service::utils::readRevitStructure("levine_2.csv");
-  cad2cav::map_service::MapServer map_server{0.05};
+  cad2cav::map_service::MapServer map_server{1};
   map_server.buildFromRevitInfo(revit_info);
   while (ros::ok()) {
     map_server.publishMap();
